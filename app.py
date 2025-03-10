5af23fb21b25e9afda216721fb71317d92f354a2
henfry-nut-cat <21035@wc.school.nz>
2025-03-10 15:04:33 +1300

    setted up db,linking up the home page and app.py

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ from flask import Flask, render_template
 import sqlite3
 from sqlite3 import Error
 
-print("idk"
+app = Flask(__name__)
+@app.route('/')
+def render_home():  # put application's code here
+    return render_template('home.html')
