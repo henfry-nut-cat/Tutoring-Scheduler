fd2dfa6c13d1b4b3ee0a9e1f19f2abab318c7b25
henfry-nut-cat <21035@wc.school.nz>
2025-03-10 14:47:56 +1300

    added templates and imported items

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 
-if __name__ == '__main__':
-    app.run()
