9163daf87d797e82a8d4e90248678eb9ed69943b
21035 <21035@wc.school.nz>
2025-03-12 09:40:19 +1300

    V3 finished off the log in and sign up page setted up student sign up connected the database together and made the database record data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sqlite3
 from sqlite3 import Error
 
@@ -13,25 +13,42 @@ def connect_to_database(db_file):
     except Error:
         print("an error has occured connecting to the database")
     return
+
 @app.route('/')
 def render_home():  # put application's code here
     return render_template('home.html')
 
-@app.route('/log_in')
+@app.route('/log_in', methods=['POST','GET'])
 def render_log_in():  # put application's code here
     return render_template('log_in.html')
 
-@app.route('/sign_up')
-def render_sign_up():  # put application's code here
+@app.route('/sign_up', methods=['POST','GET'])
+def render_sign_up():
+    if request.method =='POST':
+        fname=request.form.get('user_fname').title().strip()
+        lname=request.form.get('user_lname').title().strip()
+        email=request.form.get('user_email').lower().strip()
+        password=request.form.get('user_password')
+        password2=request.form.get('user_password2')
+        if password!=password2:
+            return redirect("/signup?error=passwords+do+not+match")
+        if len(password)<8:
+            return redirect("/signup?error=password+too+short+must+be+longer+than+8")
+        connection = connect_to_database(DATABASE)
+        query_insert="INSERT INTO Session_db(fname,lname,password,email)VALUES(?,?,?,?)"
+        cur = connection.cursor()
+        cur.execute(query_insert,(fname,lname,password,email))
+        connection.commit()
+        product_list = cur.fetchall()
+
     return render_template('sign_up.html')
 @app.route('/menu')
 def render_menu():  # put application's code here
     connection = connect_to_database(DATABASE)
-    query = "SELECT name, description, volume, image, price FROM session_db"
+    query = "SELECT First_name, Last_name, password, email FROM session_db"
     cur = connection.cursor()
     cur.execute(query)
     product_list = cur.fetchall()
-    print(product_list)
     return render_template('menu.html')
 
 
