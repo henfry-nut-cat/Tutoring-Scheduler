383a7644819e5a560ae87414cc1a853a71bb74a2
21035 <21035@wc.school.nz>
2025-03-14 14:18:33 +1300

    V4 added booking sessions made it display the loaction

--- app.py
+++ app.py
@@ -14,42 +14,62 @@ def connect_to_database(db_file):
         print("an error has occured connecting to the database")
     return
 
+
 @app.route('/')
 def render_home():  # put application's code here
     return render_template('home.html')
 
-@app.route('/log_in', methods=['POST','GET'])
+
+@app.route('/log_in', methods=['POST', 'GET'])
 def render_log_in():  # put application's code here
     return render_template('log_in.html')
 
-@app.route('/sign_up', methods=['POST','GET'])
+
+@app.route('/sign_up', methods=['POST', 'GET'])
 def render_sign_up():
-    if request.method =='POST':
-        fname=request.form.get('user_fname').title().strip()
-        lname=request.form.get('user_lname').title().strip()
-        email=request.form.get('user_email').lower().strip()
-        password=request.form.get('user_password')
-        password2=request.form.get('user_password2')
-        if password!=password2:
+    if request.method == 'POST':
+        fname = request.form.get('user_fname').title().strip()
+        lname = request.form.get('user_lname').title().strip()
+        email = request.form.get('user_email').lower().strip()
+        password = request.form.get('user_password')
+        password2 = request.form.get('user_password2')
+        if password != password2:
             return redirect("/signup?error=passwords+do+not+match")
-        if len(password)<8:
+        if len(password) < 8:
             return redirect("/signup?error=password+too+short+must+be+longer+than+8")
         connection = connect_to_database(DATABASE)
-        query_insert="INSERT INTO Session_db(fname,lname,password,email)VALUES(?,?,?,?)"
+        query_insert = "INSERT INTO Session_db(fname,lname,password,email)VALUES(?,?,?,?)"
         cur = connection.cursor()
-        cur.execute(query_insert,(fname,lname,password,email))
+        cur.execute(query_insert, (fname, lname, password, email))
         connection.commit()
         product_list = cur.fetchall()
 
     return render_template('sign_up.html')
-@app.route('/menu')
-def render_menu():  # put application's code here
+
+
+@app.route('/booking', methods=['POST', 'GET'])
+def render_booking():
+    if request.method == 'POST':
+        time_u = request.form.get('booking_time')
+        where = request.form.get('where').title().strip()
+        connection = connect_to_database(DATABASE)
+        query_insert = "INSERT INTO booking_db(date_u,location)VALUES(?,?)"
+        cur = connection.cursor()
+        cur.execute(query_insert, (time_u, where))
+        connection.commit()
+        product_list = cur.fetchall()
+
+    return render_template('booking.html')
+@app.route('/print_booking')
+def render_current_booking():
     connection = connect_to_database(DATABASE)
-    query = "SELECT First_name, Last_name, password, email FROM session_db"
+    query = "SELECT date_u,location FROM booking_db"
     cur = connection.cursor()
     cur.execute(query)
     product_list = cur.fetchall()
-    return render_template('menu.html')
+    print(product_list)
+    return render_template('show_booking.html',booking_list=product_list)
+
 
 
 if __name__ == '__main__':
