37f03e0e1c3d626e6f7af4ddfdeff77cae6146c1
henfry-nut-cat <21035@wc.school.nz>
2025-03-25 12:14:34 +1300

    fixed log_in page bad request

--- app.py
+++ app.py
@@ -6,15 +6,17 @@ from flask_bcrypt import Bcrypt
 DATABASE = 'sessions_db'
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-app.secret_key=("wdjidwdwkdwa")
+app.secret_key = ("wdjidwdwkdwa")
+
 
 def is_logged_in():
-    if (session.get('user_id') is None):
+    if session.get('user_id') is None:
         print("Not logged in")
         return False
     else:
         print("Logged in")
         return True
+
 
 def connect_to_database(db_file):
     try:
@@ -32,69 +34,42 @@ def render_home():  # put application's code here
 
 @app.route('/log_in', methods=['POST', 'GET'])
 def render_log_in():  # put application's code here
+
     if is_logged_in():
         return redirect('/booking')
     if request.method == 'POST':
-        email = request.form['email'].strip().lower()
-        password=request.form['password']
-        query="SELECT Student_id,fname,lname,password,email FROM session_db WHERE email=?"
-        con=connect_to_database(DATABASE)
-        cur=con.cursor()
-        cur.execute(query,(email))
-        user_info=cur.fetcall()
+        email = request.form['user_email'].strip().lower()
+        password = request.form['user_password']
+        query = "SELECT Student_id,fname,lname,password,email FROM session_db WHERE email=?"
+        con = connect_to_database(DATABASE)
+        cur = con.cursor()
+        cur.execute(query, (email,))
+        user_info = cur.fetcall()
         print(user_info)
         cur.close()
         try:
-            user_id=user_info[0]
-            first_name=user_info[1]
-            user_password=user_info[2]
+            user_id = user_info[0]
+            first_name = user_info[1]
+            user_password = user_info[2]
         except IndexError:
             return redirect("/login?error=email+or+password+inalid")
 
-        if not bcrypt.check_password_hash(user_password,password):
+        if not bcrypt.check_password_hash(user_password, password):
             return redirect("/login?error=email+or+password+inalid")
 
-        session["email"]=email
-        session["user_id"]=user_id
-        session["first_name"]=first_name
+        session["email"] = email
+        session["user_id"] = user_id
+        session["first_name"] = first_name
         print(session)
         return redirect("/")
 
+    return render_template('log_in.html', logged_in=is_logged_in())
 
-    return render_template('log_in.html',logged_in=is_logged_in())
 
 @app.route('/student_log_in', methods=['POST', 'GET'])
 def render_student_log_in():  # put application's code here
-    if is_logged_in():
-        return redirect('/booking')
-    if request.method == 'POST':
-        email = request.form['email'].strip().lower()
-        password=request.form['password']
-        query="SELECT Student_id,fname,lname,password,email FROM session_db WHERE email=?"
-        con=connect_to_database(DATABASE)
-        cur=con.cursor()
-        cur.execute(query,(email))
-        user_info=cur.fetcall()
-        print(user_info)
-        cur.close()
-        try:
-            user_id=user_info[0]
-            first_name=user_info[1]
-            user_password=user_info[2]
-        except IndexError:
-            return redirect("/login?error=email+or+password+inalid")
+    print(":D")
 
-        if not bcrypt.check_password_hash(user_password,password):
-            return redirect("/login?error=email+or+password+inalid")
-
-        session["email"]=email
-        session["user_id"]=user_id
-        session["first_name"]=first_name
-        print(session)
-        return redirect("/")
-
-
-    return render_template('student_log_in.html',logged_in=is_logged_in())
 
 @app.route('/sign_up', methods=['POST', 'GET'])
 def render_sign_up():
@@ -108,13 +83,13 @@ def render_sign_up():
             return redirect("/signup?error=passwords+do+not+match")
         if len(password) < 8:
             return redirect("/signup?error=password+too+short+must+be+longer+than+8")
-        hashed_password=bcrypt.generate_password_hash(password)
+        hashed_password = bcrypt.generate_password_hash(password)
         connection = connect_to_database(DATABASE)
         query_insert = "INSERT INTO Session_db(fname,lname,password,email)VALUES(?,?,?,?)"
         cur = connection.cursor()
         cur.execute(query_insert, (fname, lname, hashed_password, email))
         connection.commit()
-        return render_template('log_in.html',logged_in=is_logged_in())
+        return render_template('log_in.html', logged_in=is_logged_in())
 
     return render_template('sign_up.html')
 
@@ -131,6 +106,8 @@ def render_booking():
         connection.commit()
 
     return render_template('booking.html')
+
+
 @app.route('/print_booking')
 def render_current_booking():
     connection = connect_to_database(DATABASE)
@@ -139,7 +116,8 @@ def render_current_booking():
     cur.execute(query)
     product_list = cur.fetchall()
     print(product_list)
-    return render_template('show_booking.html',booking_list=product_list)
+    return render_template('show_booking.html', booking_list=product_list)
+
 
 @app.route('/log_out', methods=['POST', 'GET'])
 def logout():
@@ -147,6 +125,7 @@ def logout():
     session.clear()
     print(session)
     return redirect("/?message=See+you+next+time")
+
 
 if __name__ == '__main__':
     app.run()
